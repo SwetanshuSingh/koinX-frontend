c8a7682d8238f63862f0347d70bf3ae0e070a9d4
Swetanshu <swetanshusingh17@gmail.com>
2024-03-07 12:31:08 +0530

    Add : Sentiment component and events card

--- app/components/Performance.js
+++ app/components/Performance.js
@@ -1,19 +1,29 @@
+import Image from "next/image";
 import DailyPriceIndicator from "./ui/DailyPriceIndicator";
 import WeeklyPriceIndicator from "./ui/WeeklyPriceIndicator";
 
 const Performance = () => {
-    return (
-        <section className="w-[881px] h-[632px] bg-white rounded-lg p-6 flex flex-col gap-6">
-            <h2 className="text-[#0F1629] font-semibold text-2xl">Performance</h2>
+  return (
+    <section className="w-[881px] h-[632px] bg-white rounded-lg p-6 flex flex-col gap-6">
+      <h2 className="text-[#0F1629] font-semibold text-2xl leading-[28.8px]">Performance</h2>
 
-            <div className="w-full flex flex-col gap-[15px]">
-                <DailyPriceIndicator />
-                <WeeklyPriceIndicator />
-            </div>
+      <div className="w-full flex flex-col gap-[15px]">
+        <DailyPriceIndicator />
+        <WeeklyPriceIndicator />
+      </div>
 
-            
-        </section>
-    )
-}
+      <div className="flex flex-col gap-4">
+        <div className="heading flex gap-2 items-center">
+            <h3 className="text-[#44475B] text-[18.91px] leading-5 font-semibold">Fundamentals</h3>
+            <Image className="w-5 h-5" src="/assets/info.png" width={500} height={500} alt="info" />
+        </div>
+
+        <div>
+            <h3>Bitcoin Price</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Performance;
--- app/components/Sentiment.js
+++ app/components/Sentiment.js
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import EventCard from "./ui/EventCard";
+
 const Sentiment = () => {
     return (
-        <section className="w-[881px] h-[556px] bg-white rounded-lg p-6 flex flex-col gap-6">
+        <section className="max-w-[881px] h-[556px] bg-white rounded-lg p-6 flex flex-col gap-[33px]">
 
+            <div className="container w-full flex flex-col gap-[17px]">
+                <h2 className="text-[#0F1629] font-semibold text-2xl leading-[28.8px]">Sentiment</h2>
+
+                <div className="heading flex gap-2 items-center">
+                    <h3 className="text-[#44475B] text-[18.91px] leading-5 font-semibold">Key Events</h3>
+                    <Image className="w-5 h-5" src="/assets/info.png" width={500} height={500} alt="info" />
+                </div>
+
+                <div className="carousel flex gap-[14px] overflow-x-hidden">
+                    <EventCard bgColor="#E8F4FD" imageSrc="/assets/vector.png" />
+
+                    <EventCard bgColor="#EBF9F4" imageSrc="/assets/vector-2.png" />
+                </div>
+            </div>
+
+
+            
         </section>
     )
 }
