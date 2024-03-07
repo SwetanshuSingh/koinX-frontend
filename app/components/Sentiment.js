1e913b754a0c37387d3a74b58bb89e2e07f55a60
Swetanshu <swetanshusingh17@gmail.com>
2024-03-07 14:06:22 +0530

    Update : added analytics to sentiment componnent

--- app/components/Sentiment.js
+++ app/components/Sentiment.js
@@ -18,10 +18,46 @@ const Sentiment = () => {
 
                     <EventCard bgColor="#EBF9F4" imageSrc="/assets/vector-2.png" />
                 </div>
+
             </div>
-
-
             
+            <div className="w-[710px] h-[186px] flex flex-col gap-[27px]">
+                <div className="heading flex gap-2 items-center">
+                    <h3 className="text-[#44475B] text-[18.91px] leading-5 font-semibold">Analyst Estimates</h3>
+                    <Image className="w-5 h-5" src="/assets/info.png" width={500} height={500} alt="info" />
+                </div>
+
+                <div className="w-full flex items-center gap-8">
+                    <div className="w-[116.86px] h-[120px] bg-[#EBF9F4] text-[#0FBA83] rounded-full flex justify-center items-center font-medium gap-1">
+                        <h2 className=" text-[36.41px] leading-[44.06px]">76</h2>
+                        <span className="text-[16px] leading-[22px]">%</span>
+                    </div>
+
+                    <div className="w-[553.14px] h-[114px]">
+
+                        <div className="w-full h-[38px] p-2 pr-[101.16px] flex items-center gap-3">
+                            <p className="text-[#7C7E8C] font-medium text-[15px] leading-[22px]">Buy</p>
+                            <div className="bg-[#00B386] w-[349.14px] h-2 rounded-sm ml-4"></div>
+                            <p className="text-[#7C7E8C] font-medium text-[14.25px] leading-[22px]">76%</p>
+                        </div>
+
+                        <div className="w-full h-[38px] p-2 pr-[101.16px] flex items-center gap-3">
+                            <p className="text-[#7C7E8C] font-medium text-[15px] leading-[22px]">Hold</p>
+                            <div className="bg-[#C7C8CE] w-[33px] h-2 rounded-sm ml-[10px]"></div>
+                            <p className="text-[#7C7E8C] font-medium text-[14.25px] leading-[22px]">8%</p>
+                        </div>
+
+                        <div className="w-full h-[38px] p-2 pr-[101.16px] flex items-center gap-3">
+                            <p className="text-[#7C7E8C] font-medium text-[15px] leading-[22px]">Sell</p>
+                            <div className="bg-[#F7324C] w-[66.5px] h-2 rounded-sm ml-4"></div>
+                            <p className="text-[#7C7E8C] font-medium text-[14.25px] leading-[22px]">16%</p>
+                        </div>
+
+                    </div>
+                </div>   
+            </div>
+             
+
         </section>
     )
 }
